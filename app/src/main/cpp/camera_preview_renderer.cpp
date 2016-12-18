bec885465b2080b3edae939b1ad5897da7e95ebe
seungtaek.lim <devlsit81@gmail.com>
2016-12-19 00:32:18 +0900

    jni 구조 변경

--- app/src/main/cpp/camera_preview_renderer.cpp
+++ app/src/main/cpp/camera_preview_renderer.cpp
@@ -6,14 +6,52 @@
 #include <string>
 #include "logger.h"
 
-extern "C"
-void
-Java_com_st_android_device_camera_gl_ImageProcessor_test(
-        JNIEnv *env,
-        jobject /* this */) {
+#define NELEM(x) ((int)(sizeof(x)/sizeof((x)[0])))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void test(JNIEnv *env, jobject /* this */) {
 
     std::string hello = "Hello from C++";
     printf("%s", hello.c_str());
 
     LOGD("hello");
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+int jniRegisterNativMethod(JNIEnv* env, const char* className, const JNINativeMethod* gMethods, int numMethods ) {
+    jclass clazz;
+
+    clazz = env->FindClass(className);
+
+    if(clazz == NULL){
+        return -1;
+    }
+
+    if(env->RegisterNatives(clazz, gMethods, numMethods) < 0){
+        return -1;
+    }
+    return 0;
+}
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env = NULL;
+    jint result = -1;
+
+    static JNINativeMethod methodList[] = {
+            /* name, signature, funcPtr */
+            {"test", "()V", (void*)test}
+    };
+
+    if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK){
+        return result;
+    }
+
+    jniRegisterNativMethod(env, "com/st/android/device/camera/gl/ImageProcessor", methodList, NELEM(methodList));
+
+    return JNI_VERSION_1_6;
 }
